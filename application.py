5a1c639f3cd6be04cb3a0a322723165bc0827604
addiseshetu <addiseshetu@gmail.com>
2020-07-29 12:42:04 -0400

    added a python program

---
+++ application.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, session, request
+from flask_session.__init__ import Session
+
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if session.get("notes") is None:
+        session["notes"] = []
+    if request.method == "POST":
+        if request.form.get('notes'):
+            note = request.form.get("note")
+        if request.form.get('note'):
+         session["notes"].append(note)  
+    
+    return render_template("index.html", notes=session["notes"])
+    
+
+    
+    
+    
+
